8a1c58ab4e3d2da0cda90591ea8ad25293ce42ee
Anurag <anuragtiwariji2012@gmail.com>
2026-05-22 15:48:31 +0530

    added login

---
+++ tests/login.spec.ts
@@ -0,0 +1,6 @@
+import{tests, expect} from '@playwright/test';
+
+To know all the branch - give git branch -a
+For opening the vs code directly 
+go to the repo in the URl path type cmd and type code .
+
